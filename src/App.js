1f2650b3f2a0baa69e0b312e287c9653b5649c90
PranjaliDubey <pranjalidubey751@gmail.com>
2021-07-03 16:50:34 +0530

    first reactApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./NavBar";
+import Carousal from "./Carousal"
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+<Navbar>
+    </Navbar>
+    <h2>Welcome to Nature world</h2>
+    <Carousal>
+        </Carousal>
+        </div>
+);
 }
 
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return ( 
+      <nav className="navbar navbar-light bg-light">
+      <h4 className="navbar-brand">Nature</h4>
+    </nav>
+    )
+}
